1e33591bbde50bdbeb0fdd2b582b16dceb416ece
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2024-02-29 18:45:43 +0300

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,46 @@
+import path from "node:path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return {
+      ...config,
+      resolve: {
+        alias: {
+          ...config.resolve.alias,
+          svelte: path.resolve("node_modules", "svelte/src/runtime"), // Svelte 3: path.resolve('node_modules', 'svelte')
+        },
+        extensions: [...config.resolve.extensions, ".svelte"],
+        mainFields: [
+          ...config.resolve.mainFields,
+          "svelte",
+          "browser",
+          "module",
+          "main",
+        ],
+        conditionNames: ["svelte", "browser", "import"],
+      },
+      module: {
+        rules: [
+          ...config.module.rules,
+          {
+            test: /\.(html|svelte)$/,
+            use: "svelte-loader",
+          },
+          {
+            // required to prevent errors from Svelte on Webpack 5+, omit on Webpack 4
+            test: /node_modules\/svelte\/.*\.mjs$/,
+            resolve: {
+              fullySpecified: false,
+            },
+          },
+        ],
+      },
+    };
+  },
+};
 
 export default nextConfig;
